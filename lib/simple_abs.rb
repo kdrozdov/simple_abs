7d05a140451a632e6913bebe26c958eb320930c7
Kirill Drozdov <kirill.dzv@gmail.com>
2018-06-15 11:27:29 +0500

    Added weights

--- lib/simple_abs.rb
+++ lib/simple_abs.rb
@@ -12,32 +12,44 @@ module SimpleAbs
       return false
     end
   end
-  
+
   def ab_test(name, tests)
-    
-    if is_bot?
-      test_value = tests[rand(tests.size)]
-      return test_value
-    end
-    
-    if params[:test_value]
-      return params[:test_value]
-    end
-    
+    alternatives = tests.map { |t| t.is_a?(Hash) ? t : { t => 1 } }
+
+    return choose_alternative(alternatives) if is_bot?
+    return params[:test_value] if params[:test_value]
+
     test_value = cookies[name]
-    
-    if test_value.blank? || !tests.include?(test_value)
-      test_value = tests[rand(tests.size)]
+
+    if test_value.blank? || !alternatives.map { |a| a.keys.first }.include?(test_value)
+      test_value = choose_alternative(alternatives)
       cookies.permanent[name] = test_value
-      
+
       find_or_create_by_experiment_and_which_method(name, test_value).increment!(:participants)
     end
-    
+
     return test_value
   end
-  
+
+  def participate_in_test(name, test_value)
+    return if is_bot? || cookies[name].present?
+    cookies.permanent[name] = test_value
+    find_or_create_by_experiment_and_which_method(name, test_value).increment!(:participants)
+  end
+
+  def choose_alternative(tests)
+    weights = tests.map { |test| test.values.first }
+
+    total = weights.inject(:+)
+    point = rand * total
+
+    tests.zip(weights).each do |test, weight|
+      return test.keys.first if weight >= point
+      point -= weight
+    end
+  end
+
   def converted!(name)
-  
     if !is_bot?
       test_value = cookies[name]
       if test_value && cookies[name.to_s + "_converted"].blank?
@@ -58,15 +70,16 @@ module SimpleAbs
     end
 
     return alternative
-
   end
 
 
   class Railtie < Rails::Railtie
-    initializer "simple_abs.initialize" do 
+
+    initializer "simple_abs.initialize" do
       ActionView::Base.send :include, SimpleAbs
       ActionController::Base.send :include, SimpleAbs
     end
+
   end
 
   class Alternative < ActiveRecord::Base
@@ -81,6 +94,7 @@ module SimpleAbs
     def error
 
     end
+
   end
 
 
